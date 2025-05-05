72d238591105e87660d3a4e0ce0b2d6dbb14ace7
carlos-trigo <xeitodj@gmail.com>
2025-05-05 23:47:54 +0200

    update isMobile hook

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,19 +1,33 @@
-import * as React from "react"
+import * as React from "react";
 
-const MOBILE_BREAKPOINT = 768
+const MOBILE_BREAKPOINT = 768;
+const MOBILE_DENSITY = 3;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
+    undefined
+  );
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
+    const width = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+    const density = window.matchMedia(
+      `(-webkit-min-device-pixel-ratio:  ${MOBILE_DENSITY})`
+    );
 
-  return !!isMobile
+    const onChange = () => {
+      setIsMobile(
+        window.innerWidth < MOBILE_BREAKPOINT ||
+          window.devicePixelRatio === MOBILE_DENSITY
+      );
+    };
+    width.addEventListener("change", onChange);
+    density.addEventListener("change", onChange);
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    return () => {
+      width.removeEventListener("change", onChange);
+      density.removeEventListener("change", onChange);
+    };
+  }, []);
+
+  return !!isMobile;
 }
