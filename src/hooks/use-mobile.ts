ba6818b882427a1cda4e6613d9cad0537ba4cd47
carlos-trigo <xeitodj@gmail.com>
2025-05-06 19:51:40 +0200

    use navigator in useMobile hook

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,33 +1,47 @@
-import * as React from "react";
+// const MOBILE_BREAKPOINT = 768;
+// const MOBILE_DENSITY = 3;
 
-const MOBILE_BREAKPOINT = 768;
-const MOBILE_DENSITY = 3;
+import { useEffect, useState } from "react";
 
-export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
-    undefined
-  );
+// export function useIsMobile() {
+//   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
+//     undefined
+//   );
 
-  React.useEffect(() => {
-    const width = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
-    const density = window.matchMedia(
-      `(-webkit-min-device-pixel-ratio:  ${MOBILE_DENSITY})`
-    );
+//   React.useEffect(() => {
+//     const width = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+//     const density = window.matchMedia(
+//       `(-webkit-min-device-pixel-ratio:  ${MOBILE_DENSITY})`
+//     );
 
-    const onChange = () => {
-      setIsMobile(
-        window.innerWidth < MOBILE_BREAKPOINT ||
-          window.devicePixelRatio === MOBILE_DENSITY
-      );
-    };
-    width.addEventListener("change", onChange);
-    density.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    return () => {
-      width.removeEventListener("change", onChange);
-      density.removeEventListener("change", onChange);
-    };
-  }, []);
+//     const onChange = () => {
+//       setIsMobile(
+//         window.innerWidth < MOBILE_BREAKPOINT ||
+//           window.devicePixelRatio === MOBILE_DENSITY
+//       );
+//     };
+//     width.addEventListener("change", onChange);
+//     density.addEventListener("change", onChange);
+//     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+//     return () => {
+//       width.removeEventListener("change", onChange);
+//       density.removeEventListener("change", onChange);
+//     };
+//   }, []);
 
-  return !!isMobile;
-}
+//   return !!isMobile;
+// }
+
+export const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    if (navigator.maxTouchPoints > 1) {
+      // browser supports multi-touch
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  });
+
+  return isMobile;
+};
