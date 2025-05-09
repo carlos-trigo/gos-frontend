ae4416f6ca673f81b6586e01fd4e4244459c9c0c
carlos-trigo <xeitodj@gmail.com>
2025-05-09 15:25:13 +0200

    adjust header layout

--- src/components/custom/layout/header.tsx
+++ src/components/custom/layout/header.tsx
@@ -1,33 +1,33 @@
 import { User } from "@auth0/auth0-react";
 import { BigTitle } from "../text";
 import { UserDropdownMenu } from "../user/user-dropdown-menu";
-import { useIsMobile } from "@/hooks/use-mobile";
+// import { useIsMobile } from "@/hooks/use-mobile";
 
 export const Header = ({ user }: { user?: User }) => {
-  const isMobile = useIsMobile();
+  // const isMobile = useIsMobile();
 
-  if (isMobile) {
-    return (
-      <div className="w-screen flex-col justify-items-center">
-        <div className="flex-1 pt-10">
-          <UserDropdownMenu user={user} />
-        </div>
-        <div className="felx-1">
-          <BigTitle>game of skate</BigTitle>
-        </div>
-      </div>
-    );
-  }
-
+  // if (isMobile) {
   return (
-    <div className="w-full flex align-items-center pb-6">
-      <div className="flex-1 pl-8 pt-8 justify-self-start">
+    <div className="w-screen grid grid-cols-1 gap-y-0 gap-x-0">
+      <div className="w-screen flex items-center justify-center mt-16">
         <UserDropdownMenu user={user} />
       </div>
-      <div className="flex-8 text-center justify-self-center">
+      <div className="w-screen flex items-center justify-center">
         <BigTitle>game of skate</BigTitle>
       </div>
-      <div className="flex-1 pr-8 pt-8 justify-self-end"></div>
     </div>
   );
+  // }
+
+  // return (
+  //   <div className="w-full flex align-items-center pb-6">
+  //     <div className="flex-1 pl-8 pt-8 justify-self-start">
+  //       <UserDropdownMenu user={user} />
+  //     </div>
+  //     <div className="flex-8 text-center justify-self-center">
+  //       <BigTitle>game of skate</BigTitle>
+  //     </div>
+  //     <div className="flex-1 pr-8 pt-8 justify-self-end"></div>
+  //   </div>
+  // );
 };
