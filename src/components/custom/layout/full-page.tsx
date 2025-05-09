790c5557352b1a589ca351ab6ce981239b33b68b
carlos-trigo <xeitodj@gmail.com>
2025-05-09 16:16:12 +0200

    edit grid layout

--- src/components/custom/layout/full-page.tsx
+++ src/components/custom/layout/full-page.tsx
@@ -41,15 +41,13 @@ export const GridLayout = ({
   title?: string;
 }) => {
   return (
-    <FullPage>
-      <div className="w-screen h-screen grid grid-cols-1 auto-rows-[5vh] gap-y-0 gap-x-0">
-        <div className="w-screen row-span-6 md:row-span-5">{header}</div>
-        <div className="w-screen row-span-1 flex-col items-center justify-center">
-          <p className="text-center text-3xl">{title}</p>
-        </div>
-        <div className="w-screen row-span-11 h-full">{content}</div>
-        <LogoMarquee footer />
+    <div className="w-screen h-dvh grid grid-cols-1 auto-rows-[5vh] gap-y-0 gap-x-0">
+      <div className="w-screen row-span-6 md:row-span-5">{header}</div>
+      <div className="w-screen row-span-1 flex-col items-center justify-center">
+        <p className="text-center text-3xl">{title}</p>
       </div>
-    </FullPage>
+      <div className="w-screen row-span-11 h-full">{content}</div>
+      <LogoMarquee footer />
+    </div>
   );
 };
